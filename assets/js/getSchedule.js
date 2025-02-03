cebf0e35cd644ac174ae2a3292b6ce0371e4d8b1
Bipin Thapa <yukichin638@gmail.com>
2025-02-03 15:34:40 +0545

    Fetch Check

--- assets/js/getSchedule.js
+++ assets/js/getSchedule.js
@@ -1,5 +1,5 @@
 let FetchedData;
-fetch('../backend/fetch_itinerary.php').then(res => res.json()).then(data=> FetchedData = data).catch(
+fetch('/backend/fetch_itinerary.php').then(res => res.json()).then(data=> FetchedData = data).catch(
   err => console.error(err)
 );
 console.log(FetchedData);
