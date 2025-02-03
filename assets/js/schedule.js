bc0c1f32182a6f1e53d11aca4b27ed60966a591e
Bipin Thapa <yukichin638@gmail.com>
2025-02-03 14:42:59 +0545

    Schedule Page Changes

--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -8,7 +8,13 @@ status.forEach((s, i) => {
     document.querySelector(`.schedule${i + 1}`).style.border = '2px solid #17f514';
     document.querySelector(`.schedule${i + 1} .schedule-status`).style.color = '#17f514';
     document.querySelector(`.schedule${i + 1} .schedule-title-status`).style.backgroundColor = '#17f514';
-    document.querySelector(`.schedule${i+1} .schedule-description`).style.backgroundColor = '#17f514';
+    document.querySelector(`.schedule${i + 1} .schedule-description`).style.backgroundColor = '#17f514';
+    document.querySelector(`.schedule${i + 1} .schedule-description`).addEventListener('mouseenter', () => {
+      document.querySelector(`.schedule${i + 1} .schedule-description`).style.backgroundColor = '#0e9c0c'
+    });
+    document.querySelector(`.schedule${i + 1} .schedule-description`).addEventListener('mouseleave', () => {
+      document.querySelector(`.schedule${i + 1} .schedule-description`).style.backgroundColor = '#17f514'
+    });
   }
   else if (s.innerText.toUpperCase() === 'UPCOMING') {
     document.querySelector(`.schedule${i + 1}`).style.border = '2px solid rgb(58, 120, 255)';
@@ -24,8 +30,9 @@ setInterval(() => {
     }
     else if (s.innerText.toUpperCase() === 'ONGOING') {
       document.querySelector(`.schedule${i + 1}`).style.border = '2px solid #17f514';
-    document.querySelector(`.schedule${i + 1} .schedule-status`).style.color = '#17f514';
-    document.querySelector(`.schedule${i + 1} .schedule-title-status`).style.backgroundColor = '#17f514';
+      document.querySelector(`.schedule${i + 1} .schedule-status`).style.color = '#17f514';
+      document.querySelector(`.schedule${i + 1} .schedule-title-status`).style.backgroundColor = '#17f514';
+      document.querySelector(`.schedule${i + 1} .schedule-description`).style.backgroundColor = '#17f514';
     }
     else if (s.innerText.toUpperCase() === 'UPCOMING') {
       document.querySelector(`.schedule${i + 1}`).style.border = '2px solid rgb(58, 120, 255)';
