efc6a4295cb4a1fd2cd65a566c8eb83b0d17c5e8
Safal-Shrestha <shressafal05@gmail.com>
2025-02-06 14:59:20 +0545

    nav issue fixed

--- nav.php
+++ nav.php
@@ -15,56 +15,37 @@
         <li>
           <a id="map-pc">Map</a>
         </li>
-        <?php
-          if(isset($_SESSION['id']))
-          {
-        ?>
-        <li>
-          <a <?php
-            if($_SESSION['role'] == 'participant')
-            { echo "class = 'hidden'";}?>id="support-pc">Support</a>
-        </li>
-        <li>
-          <a <?php
-          if($_SESSION['role'] == 'volunteer' ||$_SESSION['role'] == 'admin')
-          { echo "class = 'hidden'";}
-        ?> id="problem-pc">Problems</a>
-        </li>
-        <?php
-          }
-        ?>
+        <?php if(isset($_SESSION['id'])) { ?>
+          <li <?php if($_SESSION['role'] == 'participant') { echo "class='hidden'"; } ?>>
+            <a id="support-pc">Support</a>
+          </li>
+          <li <?php if($_SESSION['role'] == 'volunteer' || $_SESSION['role'] == 'admin') { echo "class='hidden'"; } ?>>
+            <a id="problem-pc">Problems</a>
+          </li>
+        <?php } ?>
         <li>
           <a id="about-us-pc">About Us</a>
         </li>
       </ul>
     </li>
+
+    <!-- Mobile Menu -->
     <li class="for-mobile">
-      <ul class="bar">
-        <i class="fa-solid fa-bars"></i>
-      </ul>
-      <div class="mobile">
-        <a class="schedule-mobile">Schedule</a>
-        <a class="map-mobile">Map</a>
-        <?php
-          if(isset($_SESSION['id']))
-          {
-        ?>
-        <li>
-          <a <?php
-                if($_SESSION['role'] == 'participant')
-                { echo "class = 'hidden'";}
-          ?> id="support-pc">Support</a>
-        </li>
-        <li>
-          <a <?php
-          if($_SESSION['role'] == 'volunteer' ||$_SESSION['role'] == 'admin')
-          {echo "class = 'hidden'";}
-        ?> id="problem-pc">Problems</a>
-        </li>
-        <?php
-          }
-        ?>
-        <a  class="about-us-mobile">About us</a>
+      <i class="fa-solid fa-bars"></i>
+      <div class="mobile-menu">
+        <ul>
+          <li><a class="schedule-mobile">Schedule</a></li>
+          <li><a class="map-mobile">Map</a></li>
+          <?php if(isset($_SESSION['id'])) { ?>
+            <li <?php if($_SESSION['role'] == 'participant') { echo "class='hidden'"; } ?>>
+              <a class="support-mobile">Support</a>
+            </li>
+            <li <?php if($_SESSION['role'] == 'volunteer' || $_SESSION['role'] == 'admin') { echo "class='hidden'"; } ?>>
+              <a class="problem-mobile">Problems</a>
+            </li>
+          <?php } ?>
+          <li><a class="about-us-mobile">About Us</a></li>
+        </ul>
       </div>
     </li>
   </ul>
